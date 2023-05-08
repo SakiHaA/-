20a0100db4ff533da8e6ad9ebb83d60d1369b185
SakiHaA <107413805+SakiHaA@users.noreply.github.com>
2023-05-08 16:39:04 +0000

    11日目

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,1 +1,19 @@
-console.log(document.querySelector('#colorPicker').value);
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () => {
+    //選択した色も背景色になる//
+    document.body.style.backgroundColor = color.value;
+    
+    //カラーコードを表示//
+    if (color.value === '#ffffff') {
+        text.textContent = `カラーコード: ${color.value} (white)`;
+    }   else if (color.value === '#000000') {
+        text.textContent = `カラーコード: ${color.value} (black)`;
+    }   else {
+        text.textContent = `カラーコード: ${color.value}`;
+    }
+}
+
+//カラーピッカーが変更されたらcolorBgの関数が作動する//
+color.addEventListener('input', colorBg);
