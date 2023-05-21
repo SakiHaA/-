31f978bb1a43009cd10cf070ce10b2feb19a734d
SakiHaA <107413805+SakiHaA@users.noreply.github.com>
2023-05-21 07:27:48 +0000

    16日目

--- 4-10/js/script.js
+++ 4-10/js/script.js
@@ -1,6 +1,10 @@
 const isAgreed = document.querySelector('#check');
 const btn = document.querySelector('#btn');
 
-isAgreed.addEventListener('chenge', () => {
-    btn.disabled = !isAgreed.checked;
+isAgreed.addEventListener('change', () => {
+    if (isAgreed.checked === true){
+        btn.disabled = false;
+    } else {
+        btn.disabled = true;
+    }
 });
---
+++ 4-15/js/script.js
@@ -0,0 +1,10 @@
+const getScrollPercent = () => {
+    const scrolled = window.scrolly;
+    console.log(`${scrolled}スクロールされました`);
+    
+    const pageHeight = document.documentElement.scrollHeight;
+    
+    const viewHeight = document.documentElementclientHeight;
+    
+    console.log(``)
+}
