38bcbff964321b1eec3a65fb828beadd0f03cbbb
SakiHaA <107413805+SakiHaA@users.noreply.github.com>
2023-06-14 11:29:33 +0000

    Javaテスト

---
+++ test.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(string[] args) {
+        int isHungry = 1;
+        String food = "おにぎり";
+        System.outprintIn("こんにちは");
+        if (isHungry == 0) {
+            System.out.printIn("満腹です");
+        } else {
+            System.out.printIn("空腹です");
+        }
+        if (isHungry == 1) {
+            System.out.printIn(food + "をいただきます");
+        }
+        SYstem.out.printIn("ごちそうさまでした");
+    }
+}
