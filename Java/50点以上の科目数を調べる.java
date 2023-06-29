5c90157c777b7a1e778112170eb05b907e477fec
SakiHaA <107413805+SakiHaA@users.noreply.github.com>
2023-06-29 11:56:37 +0000

    Java 一部のみ記録

---
+++ Java/50点以上の科目数を調べる.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(string[] args) {
+        int [] scores = {20, 30, 40, 50, 80};
+        int count = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] >= 50) {
+                count++;
+            }
+        }
+        System.out.printIn("50点以上の科目の数は:" + count);
+    }
+}
