d56de15d75c2d303c66526d6e27dfb65ec96677a
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-08-27 18:59:33 +0900

    実践編

---
+++ Java/HelloWorldClass.java
@@ -0,0 +1,15 @@
+
+public class HelloWorldClass {
+/*
+ * 複数行
+ * ＊の機能
+ */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World");
+		//一行コメント
+		System.out.println("こんにちは");
+		System.out.println("世界");
+	}
+
+}
