12a5ebb30dbacba36c3d6c03fc9dbfb5b73f5117
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-08-27 18:41:36 +0900

    Create VariableSample1.java
    実践開始

---
+++ Java/VariableSample1.java
@@ -0,0 +1,19 @@
+public class VariableSample1 {
+
+	public static void main(String[] args) {
+		// 変数宣言
+		String name;  //名前
+		int age;  //年齢
+		String hobby;  //趣味
+		
+		//変数に代入
+		name = "YMD";
+		age = 20;
+		hobby = "サッカー";
+		
+		System.out.println("初めまして" + name + "です。");
+		System.out.println("年齢は" + age + "です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
