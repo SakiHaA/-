d7b03e43137432398f4531464dd747fa2af5bef7
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-08-27 18:50:36 +0900

    実践編

---
+++ Java/VariableSample4.java
@@ -0,0 +1,67 @@
+
+public class VariableSample4 {
+
+	public static void main(String[] args) {
+		// byte型の変数の宣言 + 代入
+		// byteは127まで？
+		byte b = 127;
+		
+		// 表示　
+		System.out.println(b);
+		
+		// short型の変数の宣言 + 代入
+		short s = 32767;
+		
+		// 表示
+		System.out.println(s);
+		
+		// int型の変数の宣言 + 代入
+		int i = 2147483647;
+		
+		// 表示
+		System.out.println(i);
+		
+		// long型の変数の宣言 + 代入
+		// Lをつける必要あり
+		long l = 9223372036854775807L;
+		
+		// 表示
+		System.out.println(l);
+		
+		// float型の変数の宣言 + 代入
+		// 小数も可
+		// fをつける必要あり
+		float f = 3.1234568765f;
+		
+		// 表示
+		System.out.println(f);
+		
+		// bouble型の変数の宣言 + 代入
+		double d = 3.1433772373782387387;
+		
+		// 表示
+		System.out.println(d);
+		
+		// boolean型の変数の宣言 + 代入
+		// true or false
+		boolean bool = false;
+		
+		// 表示
+		System.out.println(bool);
+		
+		// char型の変数の宣言 + 代入
+		// 一文字のみ
+		// ''のみ？
+		char c = 'A';
+		
+		//表示
+		System.out.println(c);
+		
+		// String型の変数の宣言 + 代入
+		String str = "YMD";
+		
+		// 表示
+		System.out.println(str);
+	}
+
+}
