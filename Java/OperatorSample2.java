31c5d0051e2d076506ee42fba084434e1a3dc912
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-08-30 20:42:38 +0900

    実践編

---
+++ Java/OperatorSample2.java
@@ -0,0 +1,65 @@
+
+public class OperatorSample2 {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		System.out.println(x);
+		
+		x = x + 1;
+		System.out.println(x);
+		
+		x ++;
+		System.out.println(x);
+		
+		x += 10;
+		System.out.println(x);
+		
+		System.out.println("------------");
+		
+		x = 100;
+		System.out.println(x);
+		
+		x = x - 1;
+		System.out.println(x);
+		
+		x --;
+		System.out.println(x);
+		
+		x -= 10;
+		System.out.println(x);
+		
+		x = 2;
+		System.out.println(x);
+		
+		System.out.println("------------");
+		
+		x = x * 2;
+		System.out.println(x);
+		
+		x *= 2;
+		System.out.println(x);
+		
+		x = 32;
+		System.out.println(x);
+		
+		x = x / 2;
+		System.out.println(x);
+		
+		x /= 2;
+		System.out.println(x);
+		
+		System.out.println("------------");
+		
+		x = 99;
+		System.out.println(x);
+		
+		x = x % 10;
+		System.out.println(x);
+		
+		x %= 5;
+		System.out.println(x);
+		
+	}
+
+}
