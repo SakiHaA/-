1eb9783b3d8685e9904500d70e8c004c592df296
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-09-12 23:03:33 +0900

    実践編(繰り返し)

---
+++ Java/RepetitonSample2.java
@@ -0,0 +1,19 @@
+
+public class RepetitonSample2 {
+
+	public static void main(String[] args) {
+		// 変数宣言
+		int score = 0;
+		
+		do {
+			System.out.println("勉強する");
+			
+			System.out.println("テストの点数を入力してください");
+			score = new java.util.Scanner(System.in).nextInt();
+		} while (score < 80);
+		
+		System.out.println("合格しました");
+
+	}
+
+}
