2194e456c9a25016b6c2c13181296b88988c2e53
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-09-16 01:56:10 +0900

    不要ファイルの消去とjissenn

---
+++ Java/RepetitonSample4.java
@@ -0,0 +1,29 @@
+
+public class RepetitonSample4 {
+
+	public static void main(String[] args) {
+		
+		// 変数宣言
+		int score = 0;
+		int cnt = 0;
+		
+		do {
+			cnt ++;
+			System.out.println(cnt + "回目");
+			System.out.println("勉強する");
+			if (cnt == 4) {
+				System.out.println("4回目のみ勉強してテストは受けない");
+				continue;
+			}
+			
+			if (cnt == 7) {
+				System.out.println("7回目は勉強したら繰り返しを終了");
+				break;
+			}
+			System.out.println("テストの点数を入力してください");
+			score = new java.util.Scanner(System.in).nextInt();
+		} while (score < 80);
+		
+	}
+
+}
