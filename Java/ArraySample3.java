e9314b779b2aaa2aaa7048862dff124155bef199
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-09-18 16:23:20 +0900

    実践編に(途中)

---
+++ Java/ArraySample3.java
@@ -0,0 +1,26 @@
+
+public class ArraySample3 {
+
+	public static void main(String[] args) {
+		
+		// 配列の宣言、要素の生成と代入
+		int [] scores = new int [3];
+		scores [0] = 90;
+		scores [1] = 80;
+		scores [2] = 70;
+		
+		// 別の配列を宣言、先ほどの配列を代入
+		int [] test = scores;
+		
+		System.out.println("scoresの各値の値");
+		for (int i = 0; i < scores.length; i ++) {
+			System.out.println(i + "番目の値" + scores [i]);
+		}
+		
+		System.out.println("testの各値の値");
+		for (int i = 0; i < scores.length; i ++) {
+			System.out.println(i + "番目の値" + test [i]);
+		}
+	}
+
+}
