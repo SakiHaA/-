c6348accceb780fd5e0edae5c230d44a689f30c1
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-09-23 20:10:55 +0900

    実践編(配列)

---
+++ Java/ArraySample4.java
@@ -0,0 +1,18 @@
+
+public class ArraySample4 {
+
+	public static void main(String[] args) {
+		
+		int [] scores = new int [3];
+		scores [0] = 90;
+		scores [1] = 80;
+		scores [2] = 70;
+		// scores [3] = 10; コメントなくすとエラー
+		
+		scores = null;
+		
+		// scores [0] = 10; コメントなくすとエラー
+
+	}
+
+}
