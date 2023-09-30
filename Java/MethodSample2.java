1fe6ad14a2aba575e3db28e6b9627eb3103d5f45
Masaki Hatano <107413805+SakiHaA@users.noreply.github.com>
2023-09-30 20:15:50 +0900

    実践編(メソッド)

---
+++ Java/MethodSample2.java
@@ -0,0 +1,12 @@
+
+public class MethodSample2 {
+
+	public static void main(String[] args) {
+		System.out.println("引数の数" + args.length);
+		
+		for (int i = 0; i < args.length; i ++) {
+			System.out.println("引数" + i + "番目:" + args[i]);
+		}
+	}
+
+}
